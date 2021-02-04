34f52c9123a777ca51ce743953ecfa6255fec71d
Andrew Caietti <67521582+caiettia@users.noreply.github.com>
2021-02-04 18:21:54 -0500

    Upload 0.1 exercise .R and .RHistory files

---
+++ exercises/0.1/0.1_exercises_caiettia.R
@@ -0,0 +1,68 @@
+# Part 1: the means
+
+y <- c(1,	2.5,	4,	5.5,	7,	8.5,	10,	11.5,	13,	14.5)
+
+
+# calc.my.means
+# Inputs: vector of numbers
+# Outputs: vector of length 3
+#         - arithmetic mean
+#         - geometric mean
+#         - harmonic mean
+
+calc.my.means <- function(y) {
+  
+  ## Arithmetic Mean
+  i <- 1
+  n <- length(y)
+  my.sum <- 0 
+  while (i < n+1) {
+    my.sum <- my.sum + y[i]
+    i <- i + 1
+  }
+  my.arith.mean <-sum(y)/n
+  cat("Arithmetic Mean: ", my.arith.mean, "\n")
+  
+  
+  ## Geometric Mean
+  i <- 1
+  n <- length(y)
+  my.prod <- 1
+  while (i < n+1) {
+    my.prod <- my.prod*y[i]
+    i <- i + 1
+  }
+  my.geom.mean <- my.prod ** (1/n)
+  cat("Geometric Mean: ", my.geom.mean,"\n")
+  
+  
+  ## Harmonic Mean
+  i <- 1
+  n <- length(y)
+  my.sum <- 0
+  while (i < n+1) {
+    my.sum <- my.sum + (1/y[i])
+    i <- i + 1
+  }
+  my.harm.mean <- n/my.sum
+  cat("Harmonic Mean: ", my.harm.mean,'\n')
+}
+calc.my.means(y)
+
+temp.sum <- 0
+i <- 1
+n <- length(y)
+my.mean <- sum(y)/n
+while (i < n+1) {
+  temp.sum <- temp.sum + ((y[i] - my.mean)**2)
+  i <- i+1
+}
+
+my.var <- temp.sum/(n-1)
+cat("Variance: ", my.var, '\n')
+
+my.stdev <- my.var ** (1/2)
+cat("Standard Dev: ", my.stdev, '\n')
+
+my.err <- (my.var/n) ** (1/2)
+cat("Standard Error: ", my.err, '\n')
