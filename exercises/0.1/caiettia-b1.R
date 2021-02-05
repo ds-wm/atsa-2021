bb24a5a3902d51e07221389d21520b9299ed0908
Andrew Caietti <67521582+caiettia@users.noreply.github.com>
2021-02-05 15:02:50 -0500

    Update and rename 0.1_exercises_caiettia.R to caiettia-b1.R
    updated formatting to reflect necessary formatting for exercises.

--- exercises/0.1/caiettia-b1.R
+++ exercises/0.1/caiettia-b1.R
@@ -1,15 +1,10 @@
-# Part 1: the means
+#Name: Andrew Caietti
+#Class: DATA 330
+#Date: 2021-2-5
 
 y <- c(1,	2.5,	4,	5.5,	7,	8.5,	10,	11.5,	13,	14.5)
 
-
-# calc.my.means
-# Inputs: vector of numbers
-# Outputs: vector of length 3
-#         - arithmetic mean
-#         - geometric mean
-#         - harmonic mean
-
+#Question 1: Calculate the Three Different Means
 calc.my.means <- function(y) {
   
   ## Arithmetic Mean
@@ -49,7 +44,15 @@ calc.my.means <- function(y) {
   return(c(my.arith.mean, my.geom.mean, my.harm.mean))
 }
 calc.my.means(y)
+# There is a built in mean function; mean()
+means[1] - mean(y)
+# we see they calculate the same value!
 
+# There is not a built in Geometric Mean function.
+# There is not a built in Harmonic Mean function.
+# though various libraries could be imported to calculate these means
+
+#Question 2: Calculate the Variance
 calc.var <- function(y){
   temp.sum <- 0
   i <- 1
@@ -63,16 +66,29 @@ calc.var <- function(y){
   cat("Variance: ", my.var, '\n')
   return(my.var)
 }
+# There is a built-in function for this; var()
+calc.var(y) - var(y)
+# We see that these functions then calculate the same value
 
+
+#Question 3: Calculate the Standard Deviation
 calc.stdev <- function(my.var){
   my.stdev <- my.var ** (1/2)
   cat("Standard Dev: ", my.stdev, '\n')
   return(my.stdev)
 }
+# There is a built-in function for this; sd()
+my.var <- calc.var(y)
+calc.stdev(my.var) - sd(y)
+# we see that these functions calculate the same value.
 
+
+#Question 4: Calculate the Standard Error About the Mean
 calc.stderr <- function(my.var){
   n <- length(y)
   my.err <- (my.var/n) ** (1/2)
   cat("Standard Error: ", my.err, '\n')
   return(my.err)
 }
+# There is not a built in function for standard error,
+# though various libraries could be imported for this
