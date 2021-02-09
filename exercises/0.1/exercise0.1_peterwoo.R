cd97e07d0083f6057fe265d81b3b297ad9e3269e
Peter Woo <78056158+pterwoo@users.noreply.github.com>
2021-02-10 02:19:38 +0900

    Add files via upload

---
+++ exercises/0.1/exercise0.1_peterwoo.R
@@ -0,0 +1,68 @@
+#Excercise 1 
+#Peter Woo
+
+install.packages("psych")
+install.packages("plotrix")
+library("plotrix")
+library("psych")
+
+y <- c(1,	2.5,	4,	5.5,	7,	8.5,	10,	11.5,	13,	14.5)
+
+# The means 
+y <- c(1,	2.5,	4,	5.5,	7,	8.5,	10,	11.5,	13,	14.5)
+
+## Arithmetic mean
+i <- 1
+n <- length(y)
+my.sum <- 0
+while (i < n+1) {
+  my.sum <- my.sum + y[i]
+  i <- i + 1
+}
+my.arith.mean <- my.sum / n
+my.arith.mean
+mean(y)
+  
+## Geometric mean
+i <- 1
+n <- length(y)
+my.prod <- 1
+while (i < n+1) {
+  my.prod <- my.prod * y[i]
+  i <- i + 1
+}
+my.geom.mean <- my.prod ** (1/n)
+my.geom.mean
+geometric.mean(y)
+  
+## Harmonic mean
+i <- 1
+n <- length(y)
+my.sum <- 0
+while (i < n+1) {
+  my.sum <- my.sum + (1 / y[i])
+  i <- i + 1
+}
+my.harm.mean <- n / my.sum
+my.harm.mean
+harmonic.mean(y)
+
+##Variance
+my.arith.mean <- my.sum / n
+temp.sum <- 0
+
+for (num in y){
+  temp.sum <- temp.sum + (num - my.arith.mean)**2
+}
+variance <- (temp.sum/(n-1))
+variance
+
+##Standard deviation
+stand.dev <- sqrt(variance)
+stand.dev
+
+##Standard Error
+stand.err <- sqrt(variance/n)
+stand.err
+
+savehistory(file = "excercise0.1_peterwoo.Rhistory")
