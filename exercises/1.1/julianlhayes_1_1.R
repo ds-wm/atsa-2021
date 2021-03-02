d0a8a5e1937ea63f9d5193d076f8be2b84b998b4
julianlhayes <60612618+julianlhayes@users.noreply.github.com>
2021-03-02 18:46:48 -0500

    Update julianlhayes_1_1.R

--- exercises/1.1/julianlhayes_1_1.R
+++ exercises/1.1/julianlhayes_1_1.R
@@ -1,6 +1,6 @@
 # Exercise 1
 
-# Question 1
+# Question 1/2
 t <- c(0, pi/4, pi/2, 0.75*pi, pi)
 Yt1 <- sin(t)
 Yt2 <- sin(t + pi/2)
@@ -17,3 +17,18 @@ while (i < n +1) {
   cat(round(t[i], 3), round(Yt1[i], 3), "\t", round(Yt2[i], 3), "\t", round(EYt[i], 3), "\n")
   i <- i + 1
 }
+
+# Question 3
+# The ensemble mean is based on probabilities of all possible realizations, 
+# while the mean of a realization only takes that realization into account
+# without any probabilities.
+
+# Question 4
+# Add this:
+lines(
+  t,                      # x
+  Yt2,                    # y
+  col = "blue",
+  lty = 2,
+  lwd = 1
+)
