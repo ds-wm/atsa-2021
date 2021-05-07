cf736761186cd8844d8833d29762775214d80730
rays1024 <73894812+rays1024@users.noreply.github.com>
2021-05-07 06:40:54 -0400

    Add files via upload

---
+++ exercises/3.1/rays1024_3.2.R
@@ -0,0 +1,28 @@
+install.packages("tswge")
+
+library(tswge)
+
+data(dowjones2014)
+
+dow.plot<-plotts.sample.wge(dowjones2014)
+
+aic.wge(dowjones2014, p=0:12, q=0:4, type="aic")
+#p = 1
+#phi = 0.972071834113965
+#var = 12591.8956355891
+
+dow.yw <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='yw')
+
+dow.burg <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='burg')
+
+dow.mle <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='mle')
+
+dow.df <- data.frame(
+  type=c("True", "YW", "Burg", "MLE"),
+  phi1 = c(0.972071834113965, dow.yw$phi[1], dow.burg$phi[1], dow.mle$phi[1]),
+  sigma = c(12591.8956355891, dow.yw$avar, dow.burg$avar, dow.mle$avar)
+)
+dow.df
+
+# We see that the YW phi estimator is the closest to the true value.
+# The MLE sigma estimtor is the closest to the true value.
