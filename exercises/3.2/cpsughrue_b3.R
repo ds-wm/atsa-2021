7369871511efe34964e95df4079b9f19daee699c
Connor Sughrue <55301806+cpsughrue@users.noreply.github.com>
2021-05-10 16:51:58 -0400

    Update cpsughrue_b3.R

--- exercises/3.2/cpsughrue_b3.R
+++ exercises/3.2/cpsughrue_b3.R
@@ -12,8 +12,6 @@ data(dowjones2014)
 
 # fit an AR(p) model to dowjones2014
 dow.aic <- aic.wge(dowjones2014, p = 0:12, q = 0, type = "aic")
-cat('value:', dow.aic$value, '\n')
-cat('p =', dow.aic$p)
 # value: 9.480696
 # p = 1
 
@@ -23,21 +21,16 @@ plot(dowjones2014, type = 'l',
                    xlab ='Time',
                    ylab = 'Average Daily Dow Jones Price (USD)')
 
-# Sample autocorrelations
+# sample autocorrelations
 dow.acf <- acf(dowjones2014, lag.max = 25, main = 'Autocorrelations for Dow Jones Price')
 
-# Periodogram
+# periodogram
 dow.pgram <- period.wge(dowjones2014)
 dow.parzen <- parzen.wge(dowjones2014)
 
-# Yule Walker estimator
-dow.yw <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='yw')
-
-# Burg estimator
-dow.burg <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='burg')
-
-# Maximum-likelihood estimator
-dow.mle <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='mle')
+dow.yw <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='yw') # Yule Walker estimator
+dow.burg <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='burg') # Burg estimator
+dow.mle <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='mle') # Maximum-likelihood estimator
 
 # compare the three parameter estimation methods
 dow.df <- data.frame(
