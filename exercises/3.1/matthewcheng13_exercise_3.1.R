c90a15ff524d7b9d79185b037fd13a54cbdae813
Matthew Cheng <53637086+matthewcheng13@users.noreply.github.com>
2021-05-11 18:24:33 -0400

    Add files via upload
    Fixed issues with dataset mixing and detrending

--- exercises/3.1/matthewcheng13_exercise_3.1.R
+++ exercises/3.1/matthewcheng13_exercise_3.1.R
@@ -1,7 +1,7 @@
 # Exercise 3.1:
 # Matthew Cheng
 
-install.packages("tswge")
+# install.packages("tswge")
 library(tswge)
 
 # 3.1.1 : 
@@ -39,9 +39,19 @@ co2.decomp <- stl(co2.ts, 'periodic')
 co2.trend <- co2.decomp$time.series[, 'trend']
 
 #seasonal
+
+# old code that has been fixed
+#plot(data$decidate[1:(length(co2.trend))], 
+#     data$moco2[1:(length(co2.trend))],
+#     type = 'l',
+#     col = 'red',
+#     lwd = 2,
+#     main = "Atmospheric CO2 at Mauna Loa Observatory",
+#     xlab = "Year",
+#     ylab = "parts per million (ppm)")
+
 options(repr.plot.width=12, repr.plot.height=4, repr.plot.res = 125)
-plot(data$decidate[1:(length(co2.trend))], 
-     data$moco2[1:(length(co2.trend))],
+plot(co2,
      type = 'l',
      col = 'red',
      lwd = 2,
@@ -52,15 +62,18 @@ plot(data$decidate[1:(length(co2.trend))],
 lines(co2.trend,lwd=2)
 text(1975, 362, 'Scripps Institution of Oceanography \nNOAA Global Monitoring Laboratory', cex = 1)
 
-co2.deseason <- data$moco2[1:length(co2.trend)] - co2.trend
+#co2.deseason <- data$moco2[1:length(co2.trend)] - co2.trend
 
 #deseasonalized
 options(repr.plot.width=12, repr.plot.height=10, repr.plot.res = 125)
-plot(co2.deseason, 
+plot(co2.trend, 
      main="Deseasonalized Atmospheric CO2\nat Mauna Loa Observatory",
      xlab = "Year", 
-     ylab = "Trend Offset"
+     ylab = "Trend"
 )
+
+# old code that has been fixed
+#plot(co2.deseason, main="Deseasonalized Atmospheric CO2\nat Mauna Loa Observatory",xlab = "Year", ylab = "Trend Offset")
 
 
 
