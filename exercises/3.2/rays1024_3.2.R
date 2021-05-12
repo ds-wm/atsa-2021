2884c33e0a03cc3f1c462c939cb07051bb74e378
rays1024 <73894812+rays1024@users.noreply.github.com>
2021-05-12 17:09:05 -0400

    Update rays1024_3.2.R

--- exercises/3.2/rays1024_3.2.R
+++ exercises/3.2/rays1024_3.2.R
@@ -8,8 +8,8 @@ dow.plot<-plotts.sample.wge(dowjones2014)
 
 aic.wge(dowjones2014, p=0:12, q=0:4, type="aic")
 #p = 1
-#phi = 0.972071834113965
-#var = 12591.8956355891
+#phi = 0.97
+#var = 12591.90
 
 dow.yw <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='yw')
 
@@ -18,9 +18,9 @@ dow.burg <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='burg')
 dow.mle <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='mle')
 
 dow.df <- data.frame(
-  type=c("True", "YW", "Burg", "MLE"),
-  phi1 = c(0.972071834113965, dow.yw$phi[1], dow.burg$phi[1], dow.mle$phi[1]),
-  sigma = c(12591.8956355891, dow.yw$avar, dow.burg$avar, dow.mle$avar)
+  type=c("AIC", "YW", "Burg", "MLE"),
+  phi1 = c(0.97, dow.yw$phi[1], dow.burg$phi[1], dow.mle$phi[1]),
+  sigma = c(12591.90, dow.yw$avar, dow.burg$avar, dow.mle$avar)
 )
 dow.df
 
