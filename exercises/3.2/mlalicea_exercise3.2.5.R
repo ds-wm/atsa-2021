e745c078745b53108fdd05331885badf95d11ba8
Monica Alicea <55210154+mlalicea@users.noreply.github.com>
2021-05-13 21:13:39 -0400

    Add files via upload

--- exercises/3.2/mlalicea_exercise3.2.5.R
+++ exercises/3.2/mlalicea_exercise3.2.5.R
@@ -20,13 +20,15 @@ dow.mle <- est.ar.wge(dowjones2014, p=1, factor = FALSE, type='mle')
 
 # Extract the parameter estimates, true p = 0.9816
 dow.df <- data.frame(
-  type=c("True", "YW", "Burg", "MLE"),
+  type=c("AIC", "YW", "Burg", "MLE"),
   phi1 = c(0.9816, dow.yw$phi[1], dow.burg$phi[1], dow.mle$phi[1]),
   siga = c(12591.9, dow.yw$avar, dow.burg$avar, dow.mle$avar)
 )
 dow.df
 
-# Overall, the maximum likelihood estimator shows the least variance, which aligns with the class notes stating that it is best to use ML estimators in stationary models
+# Overall, the maximum likelihood estimator shows the least variance, 
+# which aligns with the class notes stating that it is best to use ML estimators in stationary models
+
 
 
 
